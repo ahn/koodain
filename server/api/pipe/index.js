c91f8134fd9ab7d282d4732fa607987717088026
Antti Nieminen <anttihn@gmail.com>
2016-02-15 17:21:10 +0200

    fix?

--- server/api/pipe/index.js
+++ server/api/pipe/index.js
@@ -2,18 +2,12 @@
 
 var express = require('express');
 
-
 var request = require('request');
-
 
 var router = express.Router();
 
-//var controller = require('./pipe.controller');
-var controller = 2;
-
 router.all('*', function(req, res) {
   var url = req.originalUrl.slice('/api/pipe/'.length);
-  console.log('bbb', req.body);
   // Is there an easy way to reroute the exact request with body etc. (but without cookies etc.)?
   // This is close enough for now:
   var req2 = request({
@@ -22,8 +16,7 @@ router.all('*', function(req, res) {
     body: req.body,
     json: true,
   });
-  return req.pipe(req2).pipe(res);
-
+  req2.pipe(res);
 });
 
 module.exports = router;
