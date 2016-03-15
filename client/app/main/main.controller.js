92e18778b63f3eafb5aec92802ea1a34fe45fcfe
Antti Nieminen <anttihn@gmail.com>
2016-03-15 09:51:00 +0200

    comments and cleaning

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -5,39 +5,49 @@
  * Main author(s):
  * Antti Nieminen <antti.h.nieminen@tut.fi>
  */
-
 'use strict';
 
 angular.module('koodainApp')
+
+  /**
+   * Controller for the main, or "lobby" view of the IDE.
+   */
   .controller('MainCtrl', function ($scope, $http, $resource, $uibModal, Notification) {
 
+    // Create a Project $resource for getting and saving projets.
     var Project = $resource('/api/projects');
+
+    // Get the list of projects for the view.
     $scope.projects = Project.query();
 
+    // Opens a new modal view for creating a new project.
     $scope.openNewProjectModal = function() {
       $uibModal.open({
         controller: 'NewProjectCtrl',
         templateUrl: 'newproject.html'
       }).result.then(function(name) {
-        console.log(name);
+        // Create new project.
         var r = new Project({name: name});
-        r.$save().then(function() {
-          $scope.projects = Project.query();
-        },function(res) {
-          Notification.error(res.data.error);
-        });
+        return r.$save();
+      }).then(function() {
+        // New project successfully saved, reload projects.
+        $scope.projects = Project.query();
+      },function(res) {
+        // Could not create the project, for some reason.
+        Notification.error(res.data.error);
       });
     };
 
   })
 
+  /**
+   * Controller for the create new project modal dialog.
+   */
   .controller('NewProjectCtrl', function ($scope, $uibModalInstance) {
     $scope.ok = function() {
-      console.log($scope.newproject.name);
       $uibModalInstance.close($scope.newproject.name);
     };
     $scope.cancel = function() {
       $uibModalInstance.dismiss('cancel');
     };
   });
-
--- client/app/project/project.controller.js
+++ client/app/project/project.controller.js
@@ -9,35 +9,60 @@
 'use strict';
 
 angular.module('koodainApp')
+
+  /**
+   * Controller for the view for editing project sources.
+   */
   .controller('ProjectCtrl', function ($scope, $stateParams, $resource, Notification, Upload, project, files, resources) {
 
+    // project, files, and resources are resolved in project.js
+
+    // The project to be edited
     $scope.project = project;
+
+    // The source files of the project
     $scope.files = files;
+
+    // The resource files of the project
     $scope.resources = resources;
 
+    // Ace editor modes
     var modelist = ace.require('ace/ext/modelist');
 
-    var url = '/api/projects/' + $stateParams.project;
+    var projectUrl = '/api/projects/' + $stateParams.project;
 
-    var editor;
-    $scope.fileClicked= function(file) {
+    // Open file for editing
+    $scope.openFile = function(file) {
       $scope.activeFile = file;
       var mode = modelist.getModeForPath(file.name);
       $scope.activeFile.mode = mode ? mode.name : null;
     };
 
+    // If there is main.js file, open it automatically at start
+    var mainJss = files.files.filter(function(f) { return f.name === 'main.js'; });
+    if (mainJss.length > 0) {
+      $scope.openFile(mainJss[0]);
+    }
+
+    // Get the editor instance on ace load
+    var editor;
     $scope.aceLoaded = function(_editor) {
       editor = _editor;
       editor.$blockScrolling = Infinity;
       editor.setOptions({fontSize: '11pt'});
     };
 
+    // Files that are currently updated, to show a spinner on the view
     $scope.updating = {};
 
-    var File = $resource(url+'/files/:name', null, {
+    var File = $resource(projectUrl+'/files/:name', null, {
       update: {method: 'PUT' }
     });
 
+    // Watch modifications to the edited file,
+    // and save the file to the backend on every modification.
+    // The "eagerness" of the saving can be controlled with the 'debounce'
+    // attribute in the view.
     $scope.$watch('activeFile.content', function() {
       var f = $scope.activeFile;
       if (f) {
@@ -46,25 +71,27 @@ angular.module('koodainApp')
       }
     });
 
+    // Upload a source file or resource file to the given backend url
     function upload(file, toUrl) {
       return Upload.upload({
         url: toUrl,
         data: {file: file},
       }).then(function() {
-        console.log(file);
         Notification.success('Uploaded ' + file.name);
       });
     }
 
+    // Upload a source file
     $scope.uploadFile = function(file) {
-      var toUrl = url + '/files';
+      var toUrl = projectUrl + '/files';
       upload(file, toUrl).then(function() {
         $scope.files = $resource(toUrl).get();
       });
     };
 
+    // Upload a resource file
     $scope.uploadResource = function(file) {
-      var toUrl = url + '/files/resources';
+      var toUrl = projectUrl + '/files/resources';
       upload(file, toUrl).then(function() {
         $scope.resources = $resource(toUrl).get();
       });
