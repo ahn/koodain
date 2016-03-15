24e48bf82ad6c050b54e34a3ae6110292d0b3634
Antti Nieminen <anttihn@gmail.com>
2016-03-15 11:02:03 +0200

    cleaning

--- server/api/pipe/index.js
+++ server/api/pipe/index.js
@@ -6,6 +6,13 @@ var request = require('request');
 
 var router = express.Router();
 
+/**
+ * Pipe the HTTP request to the URL given.
+ *
+ * Eg. '/api/pipe/http://example.com
+ * will be piped to http://example.com
+ */
+// TODO: Security issue: it's not a good idea to reroute anything without any checks...
 router.all('*', function(req, res) {
   var url = req.originalUrl.slice('/api/pipe/'.length);
   // Is there an easy way to reroute the exact request with body etc. (but without cookies etc.)?
